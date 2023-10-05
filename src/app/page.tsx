27c380fc2b2e09d5207c8495af1ead46705ceb5a
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-10-05 16:48:53 +0200

    6er commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Button, Feature } from "@/types";
 import { FaCheck } from "react-icons/fa/index.js";
 import Image from 'next/image';
 import Gallery from'../components/Gallery';
+import Pattern from '../../public/images/Pattern02.svg'
 
 const Home = () => {
   const homepage = getListPage("homepage/_index.md");
@@ -29,37 +30,39 @@ const Home = () => {
  
  <main className=" h-screen bg-[url('/images/grid.svg')]  bg-repeat  bg-[#3c3f40] mb-10 mx-auto grid grid-cols-3 grid-rows-[85vh_20vh_20vh_20vh] md:grid-cols-5 md:grid-rows-[89vh_20vh_45vh_60vh_25vh] lg:grid-cols-8 lg:grid-rows-[25vh_66vh_45vh_25vh_25vh] xl:grid-cols-12 xl:grid-row-[25vh_66vh_30vh_25vh_25vh] gap-3">
  <section className=" tile gap-2  col-start-1 col-end-4 row-start-1 row-end-2 md:col-start-1 md:col-end-4  md:row-start-1 md:row-end-2  md:h-auto lg:w-full lg:col-start-1 lg:col-end-13">
-      <div className=" grid gap-y-6 grid-cols-[33vw_33vw_33vw] grid-rows-[27vh_33vh_33vh] md:grid-cols-[20vw_20vw_20vw_20vw_20vw] md:grid-rows-[27vw_33vh_33vh]">
-          <div className="p-3 col-start-1 col-end-4 row-start-1 row-end-2 md:bg-blue-600/25 md:col-start-1 md:col-end-6 md:row-start-1 md:row-end-2">
-          <p className=" py-2 px-2 text-3xl  md:mb-10 md:w-8/12 md:text-[5rem] leading-9 font-bold text-stone-100 lg:px-7 lg:text-[5rem]">Timeless</p>
+      <div className=" grid gap-y-6 grid-cols-[33vw_33vw_33vw] grid-rows-[27vh_33vh_33vh] md:grid-cols-[20vw_20vw_20vw_20vw_20vw] md:grid-rows-[35vw_50vh_33vh]">
+          <div className="p-3 col-start-1 col-end-4 row-start-1 row-end-2 md: bg-gradient-to-b from-blue-600/25 to-[#3c3f40] md:col-start-1 md:col-end-6 md:row-start-1 md:row-end-2">
+          <p className=" py-2 px-2 text-3xl  md:mb-10 md:w-8/12 md:py-6 md:text-[5rem] leading-9 font-bold text-stone-100 lg:px-7 lg:text-[5rem]">Timeless</p>
           <p className="px-2 leading-3 md:leading-10 text-6xl  md:text-[9rem] font-extrabold text-[#d8cdab] lg:text-[13rem] mt-3">Design</p>
           </div>
 
 
-          <div className="col-start-3 col-end-4 row-start-1 row-end-2  py-3 leading-3 font-bold text-[.75rem] mt-5 rounded-md font-primary text-stone-400  text-right md:col-start-4 md:col-end-6 md:row-start-1 md:row-end-2 md:text-[1.223rem] lg:text-[1.3333rem] mr-3">
-
-          <p className=" "
+          <div className="col-start-3 col-end-4 row-start-1 row-end-2  py-3 leading-3 font-bold text-[.75rem] mt-5 rounded-md font-primary text-stone-400  text-right md:col-start-4 md:col-end-6 md:row-start-1 md:row-end-2 md:ml-24 w-full md:text-[1.523rem] md:leading-5 lg:text-[1.3333rem] md:mr-0">
+          <p className="w-6/12 "
         dangerouslySetInnerHTML={markdownify(banner.content ?? "")}/>
        <p className=" font-phudu px-1 py-1 lg:mt-3 lg:ml-5 text-[.67rem] w-9 bg-slate-900/60 leading-3 shadow-xl shadow-gray-500 rounded-full text-center font-medium font-phudu text-orange-500 md:w-12 md:mr-5 lg:w-24 lg:text-xs">carl sagan</p>
        </div>
 
 
-      <div className="col-start-2 col-end-3 row-start-2 row-end-3 md:w-full mx-auto justify-center items-start lg:hidden">
-        <div className="w-[43vw] h-60 flex flex-col items-center justify-center">
+      <div className="col-start-2 col-end-3 row-start-2 row-end-3 md:w-full mx-auto justify-b md:items-center lg:hidden">
+        <div className="w-[43vw] md:h-full flex flex-col items-center md:justify-center md:ml-16">
             <Image
               src="/images/LogoEZ.png"
               height="350"
               width="350" 
              alt="Logo"
-              className="px-2 transform scale-150 shadow-xl shadow-gray-500  rounded-2xl"
-       
-            />
+              className="px-2 py-5 transform scale-150 md:scale-150 shadow-xl shadow-gray-500  rounded-2xl"
+       />
         </div>     
       </div>
-     
-      <div className="mt-5 col-start-2 col-end-3 row-start-3 row-end-4 mx-auto md:col-start-3 md:col-end-4 md:row-start-3 md-row-end-4" >
+
+    <div className=" hidden md:col-start-1 md:col-end-6 md:row-start-2 md:row-end-4 w-full h-60   " >
+      <Image src={Pattern} alt="pattern" width="300" height="270" />
+     </div>
+
+      <div className="mt-5 col-start-2 col-end-3 row-start-3 row-end-4 mx-auto md:col-start-3 md:col-end-4 md:row-start-3 md-row-end-4 md:text-[3rem] " >
 {banner.button!.enable && (
-          <a className="btn btn-primary text-sm" href={banner.button!.link}>
+          <a className="btn btn-primary text-sm md:text-xl" href={banner.button!.link}>
             {banner.button!.label}
           </a>
         )}
